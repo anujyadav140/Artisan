129c36691db77b7602d95ee343439fc5c7ebc2bb
Anuj Yadav <anujyadav140@gmail.com>
2023-09-14 15:27:40 +0530

    added pie chart

--- lib/client.dart
+++ lib/client.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class ClientPage extends StatefulWidget {
   const ClientPage({Key? key}) : super(key: key);
@@ -86,7 +87,7 @@ class _ClientPageState extends State<ClientPage> {
         children: [
           ElevatedButton(
             onPressed: () {
-              // Handle client history button click here
+              _showClientHistoryDialog(client); // Show client history dialog
             },
             child: Text('Client History'),
           ),
@@ -107,6 +108,182 @@ class _ClientPageState extends State<ClientPage> {
             child: Text('Edit'),
           ),
         ],
+      ),
+    );
+  }
+
+  void _showClientHistoryDialog(Client client) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        double w = MediaQuery.of(context).size.width;
+        return AlertDialog(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Client History'),
+              ElevatedButton(
+                onPressed: () {
+                  // Show the statistics popup
+                  Navigator.of(context)
+                      .pop(); // Close the client history dialog
+                  _showStatisticsDialog(client);
+                },
+                child: Text('Statistics'),
+              ),
+            ],
+          ),
+          content: Container(
+            width: w * 0.4,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  for (int i = 0; i < client.visitDates.length; i++)
+                    _buildVisitCard(
+                        client.visitDates[i], client.pastServices[i]),
+                ],
+              ),
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showStatisticsDialog(Client client) {
+    // Calculate and display statistics here
+    // You can customize this dialog as needed for your statistics
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Statistics'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Client Name: ${client.name}'),
+              Text('Total Visits: ${client.visitDates.length}'),
+              SizedBox(
+                height: 200,
+                width: 300,
+                child: PieChart(
+                  PieChartData(
+                    sections: _generatePieChartSections(client),
+                  ),
+                  swapAnimationDuration:
+                      Duration(milliseconds: 150), // Optional
+                  swapAnimationCurve: Curves.linear, // Optional
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // List<PieChartSectionData> _generatePieChartSections(Client client) {
+  //   // Calculate data for PieChart sections
+  //   // You can customize this as per your statistics logic
+  //   final totalVisits = client.visitDates.length;
+  //   final pastServicesCount = client.pastServices.length;
+
+  //   // For simplicity, we'll create two sections: Visits and Past Services
+  //   final List<PieChartSectionData> sections = [
+  //     PieChartSectionData(
+  //       title: 'Visits',
+  //       value: totalVisits.toDouble(),
+  //       color: Colors.blue,
+  //       radius: 40,
+  //     ),
+  //     PieChartSectionData(
+  //       title: 'Past Services',
+  //       value: pastServicesCount.toDouble(),
+  //       color: Colors.green,
+  //       radius: 40,
+  //     ),
+  //   ];
+
+  //   return sections;
+  // }
+
+  List<PieChartSectionData> _generatePieChartSections(Client client) {
+    // Calculate data for PieChart sections based on most frequently taken services
+    // You can customize this as per your statistics logic
+
+    // Create a map to count the frequency of each service
+    final Map<String, int> serviceFrequency = {};
+
+    // Iterate through each visit's past services and count their frequency
+    for (final visitServices in client.pastServices) {
+      for (final service in visitServices) {
+        serviceFrequency[service] = (serviceFrequency[service] ?? 0) + 1;
+      }
+    }
+
+    // Sort services by frequency in descending order
+    final sortedServices = serviceFrequency.entries.toList()
+      ..sort((a, b) => b.value.compareTo(a.value));
+
+    // Create PieChart sections based on the most frequently taken services
+    final List<PieChartSectionData> sections = [];
+    final List<Color> sectionColors = [
+      Colors.blue,
+      Colors.green,
+      Colors.red,
+      Colors.orange,
+      Colors.purple,
+      // Add more colors as needed
+    ];
+
+    // Add the most frequently taken services to PieChart sections
+    for (int i = 0;
+        i < sortedServices.length && i < sectionColors.length;
+        i++) {
+      final service = sortedServices[i].key;
+      final frequency = sortedServices[i].value;
+      sections.add(
+        PieChartSectionData(
+          title: service,
+          value: frequency.toDouble(),
+          color: sectionColors[i],
+          radius: 40,
+        ),
+      );
+    }
+
+    return sections;
+  }
+
+  Widget _buildVisitCard(String visitDate, List<String> pastServices) {
+    return Card(
+      margin: EdgeInsets.all(8),
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Visit Date: $visitDate'),
+            Text('Past Services: ${pastServices.join(", ")}'),
+          ],
+        ),
       ),
     );
   }
