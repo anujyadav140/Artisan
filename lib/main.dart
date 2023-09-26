21b081fad07988c40abc897d275241e69274235c
Anuj Yadav <anujyadav140@gmail.com>
2023-09-26 14:13:56 +0530

    attendance management

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,113 @@
-import 'package:artisan/components/helper.dart';
+// import 'package:artisan/components/helper.dart';
+// import 'package:artisan/firebase_options.dart';
+// import 'package:artisan/services/authentication/auth_gate.dart';
+// import 'package:artisan/services/authentication/auth_service.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+//   runApp(ChangeNotifierProvider(
+//     create: (context) => AuthService(),
+//     child: const MyApp(),
+//   ));
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Artisan',
+//       debugShowCheckedModeBanner: false,
+//       scrollBehavior: MyCustomScrollBehavior(),
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: const AuthGate(),
+//     );
+//   }
+// }
+import 'package:artisan/attendance/homescreen.dart';
+import 'package:artisan/attendance/loginscreen.dart';
+import 'package:artisan/attendance/model/user.dart';
 import 'package:artisan/firebase_options.dart';
-import 'package:artisan/services/authentication/auth_gate.dart';
-import 'package:artisan/services/authentication/auth_service.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+import 'package:month_year_picker/month_year_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(ChangeNotifierProvider(
-    create: (context) => AuthService(),
-    child: const MyApp(),
-  ));
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Artisan',
       debugShowCheckedModeBanner: false,
-      scrollBehavior: MyCustomScrollBehavior(),
+      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const AuthGate(),
+      home: const KeyboardVisibilityProvider(
+        child: AuthCheck(),
+      ), 
+      localizationsDelegates: const [
+        MonthYearPickerLocalizations.delegate,
+      ],
     );
+  }
+}
+
+class AuthCheck extends StatefulWidget {
+  const AuthCheck({Key? key}) : super(key: key);
+
+  @override
+  _AuthCheckState createState() => _AuthCheckState();
+}
+
+class _AuthCheckState extends State<AuthCheck> {
+  bool userAvailable = false;
+  late SharedPreferences sharedPreferences;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getCurrentUser();
+  }
+
+  void _getCurrentUser() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+
+    try {
+      if (sharedPreferences.getString('employeeId') != null) {
+        setState(() {
+          User.employeeId = sharedPreferences.getString('employeeId')!;
+          userAvailable = true;
+        });
+      }
+    } catch (e) {
+      setState(() {
+        userAvailable = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return userAvailable ? const HomeScreen() : const LoginScreen();
   }
 }
